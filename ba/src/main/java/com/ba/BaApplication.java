782343794036306c188849354231aa9bcc89d61a
kuznetsovnv <kuznetsov.nv@ya.ru>
2018-06-06 01:21:25 +0300

    default
    test

--- ba/src/main/java/com/ba/BaApplication.java
+++ ba/src/main/java/com/ba/BaApplication.java
@@ -1,12 +1,15 @@
 package com.ba;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaApplication.class, args);
+		log.info("test");
 	}
 }
