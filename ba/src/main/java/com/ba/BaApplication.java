5b80476005dc7ea3ca8d58152b6a3a80909164d8
kuznetsovnv <kuznetsov.nv@ya.ru>
2018-06-06 01:08:01 +0300

    init: rename base file

--- ba/src/main/java/com/ba/BaApplication.java
+++ ba/src/main/java/com/ba/BaApplication.java
@@ -1,12 +1,12 @@
-package com.kid;
+package com.ba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoredApplication {
+public class BaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoredApplication.class, args);
+		SpringApplication.run(BaApplication.class, args);
 	}
 }
