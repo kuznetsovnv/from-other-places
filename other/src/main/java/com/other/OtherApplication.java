dd3468a22b517181c2009f06745b0e6cedf2e544
kuznetsovnv <kuznetsov.nv@ya.ru>
2018-06-07 01:41:38 +0300

    adjustments: remove test log

--- other/src/main/java/com/other/OtherApplication.java
+++ other/src/main/java/com/other/OtherApplication.java
@@ -1,15 +1,12 @@
 package com.other;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class OtherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtherApplication.class, args);
-		log.info("test2");
 	}
 }
